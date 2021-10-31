3239b75507493eaac989d10e2d8f02833343dffd
chenjiahau <chenjiahau@gmail.com>
2021-10-31 08:52:34 +0800

    Prototype.

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('pages/index', { title: 'Text mode' });
+});
+
+router.get('/express-test', function(req, res, next) {
+  res.send({ message: 'Your express is connected to react!' });
+});
+
+module.exports = router;
