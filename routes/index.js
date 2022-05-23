d5624102ea977a7c137f0cf94d2242ccc8575225
chenjiahau <ivan.chen@dlinkcorp.com>
2022-05-23 18:42:58 +0800

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var todoList = [
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Text mode' });
@@ -8,6 +11,34 @@ router.get('/', function(req, res, next) {
 
 router.get('/express-test', function(req, res, next) {
   res.send({ message: 'Your express is connected to react!' });
+});
+
+/* API */
+router.get('/api/todo', function(req, res, next) {
+  res.send({
+    code: 200,
+    data: todoList
+  });
+});
+
+router.post('/api/todo', function(req, res, next) {
+  todoList.push({
+    todo: req.body.todo,
+    timestamp: new Date().getTime()
+  });
+
+  res.send({ 
+    code: 200
+  });
+});
+
+router.delete('/api/todo/:timestamp', function (req, res, next) {
+  const timestamp = +req.params.timestamp
+  todoList = todoList.filter(todo => todo.timestamp !== timestamp)
+
+  res.send({ 
+    code: 200
+  });
 });
 
 module.exports = router;
